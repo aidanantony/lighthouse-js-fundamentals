63b2806e43ec7129cd1f6be0f4c2518e02ac4596
Aidan Antony <aidanantony65@gmail.com>
2022-02-16 12:43:51 -0700

    Printing ingredients lists with for and while loops

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+for (let i=0; i<ingredients.length; i++){
+    console.log(ingredients[i]);
+}
+
+
+for (let i=7; i>=0; i--){
+    console.log(ingredients[i])
+}
+
+let t=0
+while (t<ingredients.length){
+    console.log(ingredients[t]);
+    t++
+}
